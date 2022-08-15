5f46e827f5f8eaf6b2c0f19eb155935a894b0552
sugardon <66208081+sugardon@users.noreply.github.com>
2022-08-15 15:15:00 +0000

    Update

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -2,6 +2,4 @@
 
 # kubeval
 KUBEVAL_VERSION="v0.16.1"
-wget https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz
-tar xf kubeval-linux-amd64.tar.gz
-sudo mv kubeval /usr/local/bin
+(curl -sSL https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz | sudo tar -C /usr/local/bin -xz kubeval)
